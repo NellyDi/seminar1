4d7c5b75fdd79fd4bb1cf0eb7d9bbba3ba42125e
NellyD <dinellys@mail.ru>
2022-12-23 12:01:24 +0300

    Задача 28

---
+++ task028/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Произведение чисел = {GetProduct(num)}");
+
+int GetProduct(int number)
+{
+    int product = 1;
+    while(number > 0)
+    {
+        product *= number;
+        number--;
+    }
+    return product;
+}
