7d59d0cf7f19f0cdf5bfcd3d35cf6afe819ceb8c
NellyD <dinellys@mail.ru>
2022-12-23 14:55:39 +0300

    Задача 30

---
+++ task030/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 30: Напишите программу, которая
+// выводит массив из 8 элементов, заполненный
+// нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+void FillArray(int[] numbers)
+{
+    int length = numbers.Length;
+    int index = 0;
+    while (index < length)
+    {
+        numbers[index] = new Random().Next(0, 2);
+        index++;
+    }
+}
+
+void PrintArray(int[] num)
+{
+    int count = num.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write(num[position] + " ");
+        position++;
+    }
+}
+
+int[] array = new int[8];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
