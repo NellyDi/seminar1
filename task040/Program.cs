742fd9c87cc7a40a9fafb3e93b255a766e5e29d0
NellyD <dinellys@mail.ru>
2023-01-11 09:59:21 +0300

    Задача 40

---
+++ task040/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать треугольник с сторонами такой длины.
+
+Console.WriteLine("Введите 3 числа");
+int num1 = int.Parse(Console.ReadLine()!);
+int num2 = int.Parse(Console.ReadLine()!);
+int num3 = int.Parse(Console.ReadLine()!);
+
+Check(num1, num2, num3);
+void Check(int a, int b, int c)
+{
+    if(a+b>c && a+c>b && b+c>a)
+    Console.WriteLine("yes");
+    else 
+    Console.WriteLine("no");
+}
