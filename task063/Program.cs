f0b2be240630d4cde696ae3447a7cd8d0443c8b4
nellydi <dinellys@mail.ru>
2023-01-15 17:51:31 +0300

    Задача 63

---
+++ task063/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+Console.Write("Введте число N: ");
+int N = int.Parse(Console.ReadLine()!);
+int i = 1;
+int Natural(int num, int a)
+{
+    if(a == num) 
+    {
+        return a;
+    }
+    else 
+        Console.Write($"{a} ");
+        return Natural(num, a + 1); 
+        
+}
+
+Console.WriteLine(Natural(N, i));
